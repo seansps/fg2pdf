96e3dd4203dd5a341ea9e3a2961678695d78abbe
Sean Schnell <seansps@gmail.com>
2021-02-14 01:33:37 -0500

    Initial commit to show some details of character sheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { setConstantValue } from "typescript";
+import "./App.css";
+import CharacterSheet from "./components/CharacterSheet";
+import FileSelector from "./components/FileSelector";
 
 function App() {
+
+  const [characterData, setCharacterData] = useState<any>();
+
+  const onConvertedFile = (characterData: any) => {
+    setCharacterData(characterData);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!characterData && (
+        <>
+          <h1 className='subtitle'>To Begin, Select an XML File</h1>
+          <FileSelector onConvertedFile={onConvertedFile} />
+        </>
+      )}
+      {characterData && <CharacterSheet characterData={characterData} />}
     </div>
   );
 }
---
+++ src/components/FileSelector.tsx
@@ -0,0 +1,55 @@
+import React, { useState, ChangeEvent } from "react";
+import * as xml2js from "xml2js";
+
+interface UploadFileProps {
+  onConvertedFile: (characterData: any) => void;
+}
+
+const FileSelector = ({onConvertedFile}: UploadFileProps) => {
+  const [currentFile, setCurrentFile] = useState<File | null>();
+  const [progress, setProgress] = useState<number>(0);
+  const [message, setMessage] = useState<string | null>();
+
+  const selectFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target && event.target.files && event.target.files.length) {
+      const file = event.target.files.item(0)
+      setCurrentFile(file);
+    }
+  };
+
+  const convert = async () => {
+    if (!currentFile) return;
+    const xml = await currentFile.text();
+    xml2js.parseString(xml, (err, result) => {
+      if(err) {
+          setMessage(`Error processing XML: ${err}`);
+      }
+      else {
+        // `result` is a JavaScript object
+        onConvertedFile(result);
+      }
+    });
+  };
+
+  return (
+    <div>
+      <label className="btn btn-default">
+        <input type="file" onChange={selectFile} />
+      </label>
+
+      <button
+        className="btn btn-success"
+        disabled={!currentFile}
+        onClick={convert}
+      >
+        Convert XML
+      </button>
+
+      <div className="alert alert-light" role="alert">
+        {message}
+      </div>
+    </div>
+  );
+};
+
+export default FileSelector;
